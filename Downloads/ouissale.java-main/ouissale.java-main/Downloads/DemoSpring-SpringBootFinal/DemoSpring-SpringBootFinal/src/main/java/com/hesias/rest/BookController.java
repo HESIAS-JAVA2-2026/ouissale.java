13a4f39a5dbd3de4ace742c86d04f783d4da0b6b
ouissaale <ouissale.navarro@hesias.fr>
2026-05-21 09:30:57 +0200

    update_project

--- Downloads/ouissale.java-main/ouissale.java-main/Downloads/DemoSpring-SpringBootFinal/DemoSpring-SpringBootFinal/src/main/java/com/hesias/rest/BookController.java
+++ Downloads/ouissale.java-main/ouissale.java-main/Downloads/DemoSpring-SpringBootFinal/DemoSpring-SpringBootFinal/src/main/java/com/hesias/rest/BookController.java
@@ -25,4 +25,14 @@ public class BookController {
     public Book createBook(@RequestBody Book book) {
         return bookService.save(book);
     }
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Long id, @RequestBody Book book) {
+        book.setId(id);
+        return bookService.save(book);
+    }
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
+
 }
